62b4ad0904035a6fdef76e38bb00b56e0ea0d44c
Documenter.jl <documenter@juliadocs.github.io>
2022-07-15 12:45:44 +0000

    build based on d6bd448

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AtlasUtils","category":"page"},{"location":"#AtlasUtils","page":"Home","title":"AtlasUtils","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AtlasUtils.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AtlasUtils]","category":"page"}]
+}
