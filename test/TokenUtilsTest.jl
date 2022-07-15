d6324856a3e08409942a4e3426137cb16412968a
AlexFreik <freik.aa@phystech.edu>
2022-07-16 01:53:14 +0300

    fix getnames bug

--- test/TokenUtilsTest.jl
+++ test/TokenUtilsTest.jl
@@ -5,4 +5,5 @@ using Test
     @test TokenUtils.getnames("") == Set{Symbol}([])
     @test TokenUtils.getnames("ex1") == Set{Symbol}([:ex1])
     @test TokenUtils.getnames("ex1 + a - 5") == Set{Symbol}([:ex1, :+, :-, :a])
+    @test TokenUtils.getnames("b+1\nex1 + a - 5") == Set{Symbol}([:ex1, :+, :-, :a, :b])
 end
